fd7cc9ab318ce1dc80dfc4a48e07dd08864ef5e4
Ayushisingh0403 <ayushi.25boe10066@vitbhopal.ac.in>
2025-11-23 15:38:38 +0530

    Create auth.js

---
+++ MockMate/MockMate/backend/routes/auth.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  getAllUsers,
+  deleteUser,
+  updateResumeLink,
+} = require("../controllers/authController");
+const { protect } = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+// Basic Auth Routes
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", protect, getUserProfile);
+router.put("/resume-link", protect, updateResumeLink);
+router.get("/users", getAllUsers);
+router.delete("/users/:id", deleteUser);
+
+module.exports = router;
